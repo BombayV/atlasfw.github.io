fc54db7db62e5cf5e0f8bea97c1096355bb6390e
BombayV <OMGmsebastian@gmail.com>
2022-02-02 02:26:14 -0500

    fix(app): add anims back

--- src/App.vue
+++ src/App.vue
@@ -239,4 +239,18 @@ body {
   height: 100vh;
   background-color: #fafafa;
 }
+
+.slide-fade-enter-active {
+  transition: all 0.15s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.15s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
 </style>
