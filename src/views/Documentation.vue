51c0a9cfd065e9315da4f722945f335d5ffdf898
BombayV <OMGmsebastian@gmail.com>
2022-03-19 11:47:12 -0400

    another error

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -7,14 +7,14 @@ export default {
   name: "Documentation",
   data() {
     return {
-      web: "https://atlasfw-docs.pages.dev/en/intro",
+      web: "https://atlasfw-docs.pages.dev/#/en/intro",
     };
   },
   mounted() {
-    this.web = "https://atlasfw-docs.pages.dev/en/intro";
+    this.web = "https://atlasfw-docs.pages.dev/#/en/intro";
     const lang = localStorage.getItem("lang") || "us";
     if (lang !== "us") {
-      this.web = `https://atlasfw-docs.pages.dev/${lang}/intro`;
+      this.web = `https://atlasfw-docs.pages.dev/#/${lang}/intro`;
     }
   },
 };
